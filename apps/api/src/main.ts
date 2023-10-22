a9fc5c9a8553034fd26db383d5855bc2c120f84d
Sébastien Lombard <sebastien.lombard@cocoon-space.com>
2023-10-22 18:17:35 +0200

    refactor(api): remove swagger (#6)

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,10 +1,5 @@
-import {
-	ClassSerializerInterceptor,
-	ValidationPipe,
-	VersioningType,
-} from '@nestjs/common'
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common'
 import { NestFactory, Reflector } from '@nestjs/core'
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 import { AppModule } from './app.module'
 
@@ -14,23 +9,7 @@ const bootstrap = async () => {
 	app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }))
 	app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)))
 
-	app.enableVersioning({
-		type: VersioningType.URI,
-		defaultVersion: '1',
-	})
-
-	const config = new DocumentBuilder()
-		.setTitle('Cats example')
-		.setDescription('The cats API description')
-		.setVersion('')
-		.addTag('cats')
-		.build()
-
-	const document = SwaggerModule.createDocument(app, config)
-
-	SwaggerModule.setup('docs', app, document)
-
-	await app.listen(3000)
+	await app.listen(3001)
 }
 
 bootstrap()
